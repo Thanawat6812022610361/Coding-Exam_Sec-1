641a1367b2a23656e79636d582c5dd9a024b119c
Thanawat6812022610361 <noonmobile10947@gmail.com>
2025-10-06 14:25:05 +0700

    Add files via upload

---
+++ Sec-1_6812022610361_ThanawatBoonkor.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+ int total_hour (int hour[],int days){
+ int total = 0;
+ for (int i = 0; i < days; i++){
+    total += hour[i];
+ }
+ return 0;
+}
+// คำนวณชั่วโมงรวมป้อนค่าเวลาทั้ง + กับจำนวนชั่วโมง
+int L = 75;
+int M = 120;
+int H = 180;
+printf ("Ot_Timewithpercent =", L*20/100);
